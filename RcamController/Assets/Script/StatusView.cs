4c85867e5717e29c586e9476263dc8c6ee7c94f3
Volta Mac mini <dev@volta-xr.com>
2021-10-13 18:08:55 +0100

    Added battery level indicator and icons and tracking .a files in LFS

--- RcamController/Assets/Script/StatusView.cs
+++ RcamController/Assets/Script/StatusView.cs
@@ -7,14 +7,29 @@ sealed class StatusView : MonoBehaviour
 {
     [SerializeField] Camera _camera = null;
     [SerializeField] Text _statusText = null;
+        [SerializeField] Text _batteryLevelText = null;
 
-    public static string Format(Vector3 v)
+        public static string Format(Vector3 v)
       => $"({v.x,7:F2}, {v.y,7:F2}, {v.z,7:F2})";
 
     void Update()
-      => _statusText.text =
-           "Position: " + Format(_camera.transform.position) + "\n" +
-           "Rotation: " + Format(_camera.transform.rotation.eulerAngles);
+    {
+#if UNITY_IOS
+            var batteryLevel = SystemInfo.batteryLevel;
+            _batteryLevelText.text = (batteryLevel * 100f).ToString() + "%";
+            if (batteryLevel <= 0.3f)
+            {
+                _batteryLevelText.color = Color.red;
+            }
+            else
+            {
+                _batteryLevelText.color = Color.green;
+            }
+#endif
+            _statusText.text =
+        "Position: " + Format(_camera.transform.position) + "\n" +
+        "Rotation: " + Format(_camera.transform.rotation.eulerAngles);
+    }
 }
 
 } // namespace Rcam2
